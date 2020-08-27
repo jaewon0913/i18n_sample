03879fea4c2c79ab5ce125eaf620c83f82b7d2e1
황재원 <jw_hwang@ubivelox.com>
2020-08-27 16:33:19 +0900

    다국어 first commit

---
+++ src/main/java/com/example/i18n/I18nTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.i18n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class I18nTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(I18nTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/i18n/I18nTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.i18n;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
